77e3d2b949fba2074085c9facc4acc97b0736ee9
chenyijun2 <u11202202@o365.ttu.edu.tw>
2024-04-28 13:47:21 +0800

    1.製作個人封面(我只設計了20行) 2.用do-while寫密碼的程式 3.製作下一步的提示表格

---
+++ E1B02.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+void f(void);
+int main() 
+{ 
+	printf("EEEE  1  BBB     000      2     H  H  W   W   W   2    \n");
+   	printf("E     1  B  B   0   0   2   2   H  H  W   W   W  2  2  \n");
+   	printf("EEEE  1  BBB    0   0      2    HHHH  W   W   W    2   \n");
+   	printf("E     1  B  B   0   0    2      H  H  W   W   W   2    \n");
+   	printf("EEEE  1  BBB     000    222222  H  H   WWW WWW   2222  \n");
+	f();
+	printf("Hello\n");
+	printf("歡迎光臨\n");
+	printf("welcome\n");
+	printf("這是E1B02陳怡均的HW2\n");
+	f();
+	printf("密碼提示1\n");
+	printf("密碼是四位數\n");
+	printf("密碼提示2\n");
+	printf("密碼= @$%%!減$@& \n");
+	printf("密碼提示3\n");
+	printf("一個符號為一個數字\n");//兩個百分比是一個數字 
+	printf("只有三次機會\n");
+	printf("若輸入錯誤三次程式將會結束\n");
+	f();
+	int n,pass;
+	do
+	{
+		printf("請輸入密碼");
+		scanf("%d",&pass);
+		n++;
+		if(n>=3)
+		{
+			printf("輸入超過3次\n");
+			printf("\a");
+			break;
+		}
+	}while(pass!=2024);
+	system("CLS");
+	f();
+	printf("    直角三角形---A or a\n");
+	printf("    99乘法表-----B or b\n");
+	printf("    結束---------C or c\n");
+	f();
+
+    return 0;
+}
+void f(void)
+{
+	printf("==============================\n");
+}
+
+
+
