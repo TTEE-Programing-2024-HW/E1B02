21fdf7f61607a005fdd30ad80f37c945867ec01e
chenyijun2 <u11202202@o365.ttu.edu.tw>
2024-05-10 21:36:28 +0800

    製作個人風格畫面

---
+++ E1b02HW3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+void s(void);
+int main()
+{
+	printf("H   H    W       W    3 3   \n");
+	printf("H   H    W   W   W   3   3  \n");
+	printf("H   H    W   W   W      3   \n");
+	printf("HHHHH    W   W   W    3     \n");
+	printf("H   H    W   W   W      3   \n");
+	printf("H   H    W   W   W  3    3  \n");
+	printf("H   H      W   W     3 3    \n");
+	s();
+	printf("Welcome\n");
+	printf("This is HW3 for E1b02.\n");
+	s();
+	printf("Password hint 1\n");
+	printf("The password is four digits.\n");
+	printf("Password hint 2\n");
+	printf("the first number:-4   \n");
+	printf("the second number:xu/6\n");
+	printf("the third number:-4   \n");
+	printf("the last number:n4    \n");
+	printf("Password hint 3\n");
+	printf("hint 2 is related to Chinese\n");
+	s();
+	return 0;
+	
+}
+void s(void)
+{
+	printf("------------------------------------------\n");
+}
